3f786cb2bac0f2c8141d8fda052576a0e02fa098
Svetlana <nokturna3@gmail.com>
2025-05-28 13:27:50 +0300

    add header and header scripts

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 import '../scss/main.scss'; // No modification is possible without a selected code snippet.
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+
+useTheme();
+useBurger();
